cfa1fcf53cdb3ac60540172840188b9a9fd4ea7f
Joshua Cannon <3956745+thejcannon@users.noreply.github.com>
2025-01-09 22:03:32 -0600

    perfection

--- src/shimbboleth/_model/json_schema.py
+++ src/shimbboleth/_model/json_schema.py
@@ -20,7 +20,6 @@ T = TypeVar("T")
 ModelT = TypeVar("ModelT", bound=Model)
 
 
-
 @singledispatch
 def schema(field_type, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
     if field_type is bool:
@@ -50,7 +49,9 @@ def schema(field_type, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, An
 
 
 @schema.register
-def schema_generic_alias(field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def schema_generic_alias(
+    field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     container_t = field_type.__origin__
     if container_t is list:
         return schema_list(field_type=field_type, model_defs=model_defs)
@@ -60,17 +61,25 @@ def schema_generic_alias(field_type: GenericAlias, *, model_defs: dict[str, dict
 
 
 @schema.register
-def schema_union_type(field_type: UnionType, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
-    return {"oneOf": [schema(argT, model_defs=model_defs) for argT in field_type.__args__]}
+def schema_union_type(
+    field_type: UnionType, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
+    return {
+        "oneOf": [schema(argT, model_defs=model_defs) for argT in field_type.__args__]
+    }
 
 
 @schema.register
-def _schema_generic_union_type(field_type: GenericUnionType, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def _schema_generic_union_type(
+    field_type: GenericUnionType, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     return schema_union_type(field_type, model_defs=model_defs)
 
 
 @schema.register
-def schema_literal(field_type: LiteralType,  *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def schema_literal(
+    field_type: LiteralType, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     return {"enum": list(field_type.__args__)}
 
 
@@ -88,33 +97,44 @@ def _schema_annotation_type(annotation: Any) -> dict[str, Any]:
     else:
         raise TypeError(f"Unsupported annotation: {annotation}")
 
+
 @schema.register
-def schema_annotation(field_type: AnnotationType, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def schema_annotation(
+    field_type: AnnotationType, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     ret = schema(field_type.__origin__, model_defs=model_defs)
     for annotation in field_type.__metadata__:
         ret.update(_schema_annotation_type(annotation))
     return ret
 
 
-def schema_list(field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def schema_list(
+    field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     (argT,) = field_type.__args__
     return {"type": "array", "items": schema(argT, model_defs=model_defs)}
 
 
-def schema_dict(field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+def schema_dict(
+    field_type: GenericAlias, *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     keyT, valueT = field_type.__args__
     key_schema = schema(keyT, model_defs=model_defs)
     assert key_schema.pop("type") == "string"
     return {
         "type": "object",
-        "additionalProperties": schema(valueT, model_defs=model_defs) if valueT is not Any else True,
+        "additionalProperties": schema(valueT, model_defs=model_defs)
+        if valueT is not Any
+        else True,
         **({"propertyNames": key_schema} if key_schema else {}),
     }
 
 
 class _ModelFieldSchemaHelper:
     @staticmethod
-    def _get_field_type_schema(field: dataclasses.Field, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+    def _get_field_type_schema(
+        field: dataclasses.Field, *, model_defs: dict[str, dict[str, Any]]
+    ) -> dict[str, Any]:
         json_loader = field.metadata.get("json_loader", None)
         if json_loader:
             input_type = getattr(
@@ -128,24 +148,38 @@ class _ModelFieldSchemaHelper:
         return schema(field.type, model_defs=model_defs)
 
     @staticmethod
-    def visit_model_field(field: dataclasses.Field, *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
-        field_schema = _ModelFieldSchemaHelper._get_field_type_schema(field, model_defs=model_defs)
+    def visit_model_field(
+        field: dataclasses.Field, *, model_defs: dict[str, dict[str, Any]]
+    ) -> dict[str, Any]:
+        field_schema = _ModelFieldSchemaHelper._get_field_type_schema(
+            field, model_defs=model_defs
+        )
         if field.default is not dataclasses.MISSING:
             field_schema["default"] = field.default
         elif field.default_factory is not dataclasses.MISSING:
             field_schema["default"] = field.default_factory()
         return field_schema
 
-def schema_model(model_type: type[ModelT], *, model_defs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+
+def schema_model(
+    model_type: type[ModelT], *, model_defs: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     model_name = model_type.__name__
     if model_name not in model_defs:
         fields = tuple(field for field in dataclasses.fields(model_type) if field.init)
-        model_defs[model_name]  = {
+        model_defs[model_name] = {
             "type": "object",
             "properties": {
-                **{field.name: _ModelFieldSchemaHelper.visit_model_field(field, model_defs=model_defs) for field in fields},
                 **{
-                    name: {"$ref": f"#/$defs/{model_name}/properties/{field_alias.alias_of}"}
+                    field.name: _ModelFieldSchemaHelper.visit_model_field(
+                        field, model_defs=model_defs
+                    )
+                    for field in fields
+                },
+                **{
+                    name: {
+                        "$ref": f"#/$defs/{model_name}/properties/{field_alias.alias_of}"
+                    }
                     for name, field_alias in model_type.__field_aliases__.items()
                 },
             },
